b11bc408af1f43a9962902c24f263eef05cc39eb
BenjaRogers <rogersben219@gmail.com>
2023-03-14 00:19:33 -0400

    Change return type to Response for routes

--- greenhouse/web/routes.py
+++ greenhouse/web/routes.py
@@ -5,18 +5,18 @@
 import importlib.resources
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
-
+from starlette.responses import Response
 
 templates = Jinja2Templates(
     directory=importlib.resources.files('greenhouse.web.templates'),
 )
 
 
-async def homepage(request: Request) -> Jinja2Templates:
+async def homepage(request: Request) -> Response:
     return templates.TemplateResponse('index.html', {'request': request})
 
-async def dashboard(request: Request) -> Jinja2Templates:
+async def dashboard(request: Request) -> Response:
     return templates.TemplateResponse('dashboard.html', {'request': request})
 
-async def top_emotes(request: Request) -> Jinja2Templates:
+async def top_emotes(request: Request) -> Response:
     return templates.TemplateResponse('top-emotes.html', {'request': request})
